5c1dcda67f11755c1310b19ec83121e7cb1a1a1f
puncharat893 <pichamon09062537@gmail.com>
2026-06-14 10:14:17 +0700

    Initial commit

---
+++ Config.js
@@ -0,0 +1,9 @@
+// ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
+    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbxtuOqez9ZjFTZllVoMwsg9Kpr953n1Vm7RoxSmrlUkxZXDpoAa_VrUtbCE8bxqddLARA/exec';
+// member
+    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyPdbOtuedjpzTmmYdySRX4TCyCjMfZq6ZwBu3LAH6QiSfTUxeoMtIKTqApfhXx26uvhg/exec';
+// checkin
+//LIFF_ID
+    const LIFF_ID = '2008040462-gVP0VXlB'; // LIFF ID Checkin
+    const LIFF_HISTORY = '2008040462-q4XEpg4y'; // LIFF ID history
+    const LIFF_REGISTER = '2008040462-mP3Db7aX'; // LIFF ID register
